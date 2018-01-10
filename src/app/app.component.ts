e4380ee4ff3f6debae7c62f0147ddf0aaf21d89f
neha jain <nehajain@nehas-MacBook-Air.local>
2018-01-10 23:39:10 +0000

    added localstorage feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-// import '../assets/css/styles.css';
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -11,17 +9,20 @@ export class AppComponent {
 
   // title = "App starts";
 
-  public items : [string]= ["Angular 4", "React", "Backbone"];
+  public items : [string] = JSON.parse(localStorage.getItem("items")) || ["Angular 4", "React", "Backbone"];
   public newItem : string = "";
   public pushItem = function () {
     if (this.newItem !== "") {
       this.items.push(this.newItem);
       this.newItem = "";
+      this.setLocalStorageData();
     }
   }
-
   public removeItem = function (index : number) {
     this.items.splice(index, 1);
+    this.setLocalStorageData();
   }
-
+  private setLocalStorageData = function () {
+    localStorage.setItem('items', JSON.stringify(this.items));
+  }
 }
